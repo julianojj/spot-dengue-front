7ef5b3a02a19804ce44a7969246eefc1f3c05794
Juliano <cont.juliano@outlook.com>
2024-05-27 14:28:27 -0300

    fixed public image path

--- src/components/report/index.jsx
+++ src/components/report/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import image from '/public/image.svg';
 import { Header } from "../header";
+import image from '/image.svg';
 
 const Container = styled.div`
     padding: 24px;
